0c66d3f3b8bdbb22cd0a85e880c0985b316184ce
Maarten Engelen <maarten@iridia.nl>
2019-10-10 17:38:57 +0200

    Few quick changes to make the skyplot naming work for us

--- src/module.ts
+++ src/module.ts
@@ -170,7 +170,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
     let cfg = this.panel.pconfig;
     this.axis = [
       {
-        disp: 'Angle',
+        disp: 'Azimuth',
         idx: 1,
         metric: name => {
           if (name) {
@@ -180,7 +180,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
         },
       },
       {
-        disp: 'Distance',
+        disp: 'Elevation',
         idx: 2,
         metric: name => {
           if (name) {
@@ -419,7 +419,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
         let petals = [];
 
         // find max wind speed and speed levels
-        let max_speed = Math.max(...r);
+        let max_speed = 90;
         let bin_size = cfg.settings.wind_speed_interval; //  max_speed / bin_num);
         let bin_num = Math.ceil(max_speed / bin_size);
         let speed_levels = [];
@@ -479,7 +479,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
           let lower_level = speed_levels[bin_idx];
           let upper_level = speed_levels[bin_idx + 1];
           trace['name'] =
-            lower_level.toString() + ' - ' + upper_level.toString() + ' m/s';
+            lower_level.toString() + ' - ' + upper_level.toString() + '°';
           trace['type'] = 'scatterpolar';
           trace['mode'] = 'lines';
           trace['theta'] = thetas;
@@ -542,7 +542,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
     this.panel.pconfig.layout.showlegend = legendmapping[plot_type];
 
     let radialaxismapping = {
-      scatter: {ticksuffix: ' m/s', angle: 90},
+      scatter: {ticksuffix: '°', angle: 90},
       windrose: {ticksuffix: '%', angle: 90},
     };
     this.panel.pconfig.layout.polar.radialaxis = radialaxismapping[plot_type];
